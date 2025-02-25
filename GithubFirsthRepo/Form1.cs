078d90d876418118b485d2349638e2fa6daafa76
Deniz Yildiz <148767810+denizyildiz0@users.noreply.github.com>
2025-02-26 00:02:27 +0300

    Form load mesaj kutusu eklendi

--- GithubFirsthRepo/Form1.cs
+++ GithubFirsthRepo/Form1.cs
@@ -6,5 +6,15 @@ namespace GithubFirsthRepo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+        }
     }
 }
