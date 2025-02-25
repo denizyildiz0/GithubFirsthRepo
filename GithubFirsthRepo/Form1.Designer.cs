c7ef16d9dd0aef46aa2611e077af4fe9fc4f6692
Deniz Yildiz <148767810+denizyildiz0@users.noreply.github.com>
2025-02-26 00:06:12 +0300

    Kaydedt butonu altında validation kontrolü yapıldı.

--- GithubFirsthRepo/Form1.Designer.cs
+++ GithubFirsthRepo/Form1.Designer.cs
@@ -35,9 +35,9 @@
             // 
             button1.Location = new Point(48, 47);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(130, 49);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Kaydet";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
--- GithubFirsthRepo/Form1.cs
+++ GithubFirsthRepo/Form1.cs
@@ -9,7 +9,12 @@ namespace GithubFirsthRepo
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string username = "Deniz Yýldýz";
 
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new Exception("Hata");
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
